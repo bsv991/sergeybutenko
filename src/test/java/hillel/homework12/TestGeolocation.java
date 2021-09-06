886cb7f94fa84d70960c1f887033e0ad8c5d96cc
Sergey Butenko <sergej.butenko@privatbank.ua>
2021-09-06 17:44:59 +0300

    add test conditional wait in test class TestGeolocation.java

--- src/test/java/hillel/homework12/TestGeolocation.java
+++ src/test/java/hillel/homework12/TestGeolocation.java
@@ -1,14 +1,16 @@
 package hillel.homework12;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class TestGeolocation extends BaseTest {
     @Test
-    public void testGeolocation () throws InterruptedException {
+    public void testGeolocation () {
         driver.get("https://the-internet.herokuapp.com/geolocation");
+        driver.manage().window().maximize();
         driver.findElement(By.xpath("//*[@id=\"content\"]/div/button")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"lat-value\"]"))).getText();
         String latValue = driver.findElement(By.xpath("//*[@id=\"lat-value\"]")).getText();
         String longValue = driver.findElement(By.xpath("//*[@id=\"long-value\"]")).getText();
         System.out.println("Latitude: " + latValue);
